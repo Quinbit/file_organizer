4ac6ce4bf7494a35d03af495a32df5d34336aa29
Quin <quinsykora@gmail.com>
2018-06-26 10:41:17 -0400

    First set of files and setup for the dependencies

---
+++ categorize/main.py
@@ -0,0 +1,24 @@
+import os
+import pygame
+
+def main():
+    response = 0
+
+    while type(response) != str:
+        print("We are currently in the directory " + os.getcwd())
+        print("Would you like to turn this into a categorization directory? (y/N)")
+        response = input()
+
+        if type(response) != str:
+            print("Please only input a character")
+
+    response = response.lower()[0]
+
+    if response != 'y':
+        print("Ending program")
+        return
+
+    os.system("mkdir .files")
+
+if __name__ == "__main__":
+    main()
