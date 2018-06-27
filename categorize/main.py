8956fc3bec0679dfb9be84583b5fe79434e94097
Quin <quinsykora@gmail.com>
2018-06-27 12:03:50 -0400

    Working directory changing

--- categorize/main.py
+++ categorize/main.py
@@ -11,6 +11,18 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget,QVBoxLayout, QMessageBox
 from random import random
 
+def parse_for_hidden(files):
+    deleted = 0
+    for i in range(len(files)):
+        if len(files[i-deleted]) < 1:
+            del files[i-deleted]
+            deleted += 1
+        elif files[i-deleted][0]=='.':
+            del files[i-deleted]
+            deleted += 1
+
+    return files
+
 class App(QWidget):
     def __init__(self):
         #here we initialize the basic parameters for the gui
@@ -22,8 +34,9 @@ class App(QWidget):
         self.top = 10
         self.width = 1200
         self.height = 800
-        self.base_dir = os.getcwd()
-        self.title_font = QtGui.QFont("Times", 20, QtGui.QFont.Bold)
+        self.cur_dir = os.getcwd()
+        self.title_font = QtGui.QFont("Times", 15, QtGui.QFont.Bold)
+        self.base_dir = ""
 
         self.initUI()
 
@@ -46,17 +59,25 @@ class App(QWidget):
         self.button = button
         self.drag_object = drag_object
 
-
         self.cur_file = ""
         self.selected_file = QLabel(self)
         self.selected_file.setText("The selected file is: " + self.cur_file)
+        self.selected_file.setFont(self.title_font)
         self.selected_file.move(500, 100)
+
+        self.directory_label = QLabel(self)
+        self.directory_label.setText("Root Directory: " + self.base_dir)
+        self.directory_label.move(500, 40)
+
+        self.change_dir = QPushButton("Change Directory", self)
+        self.setToolTip("Click to change root directory")
 
         l = QLabel(self)
         l.setText("Files/Directories")
         l.move(100,100)
 
         self.show()
+        self.getBaseDir()
 
     @pyqtSlot()
     def on_click(self):
@@ -79,6 +100,17 @@ class App(QWidget):
         else:
             return "Error"
 
+    def getBaseDir(self):
+        if not os.path.isdir(os.getcwd() + "/.hidden"):
+            self.buildDirPopup()
+        else:
+            self.base_dir = os.getcwd()
+            self.directory_label.setText("Root Directory: " + self.base_dir)
+            self.directory_label.adjustSize()
+
+    def buildDirPopup(self):
+        self.dirPopup = DirectoryPopup(self)
+
 class CustomLabel(QLabel):
     def __init__(self, title, parent):
         super().__init__(title, parent)
@@ -87,7 +119,7 @@ class CustomLabel(QLabel):
         self.setAcceptDrops(True)
         self.setGeometry(20, 20, 300, 500)
         self.setStyleSheet("border:1px solid rgb(0, 0, 0);")
-        files = os.listdir(os.getcwd())
+        files = parse_for_hidden(os.listdir(os.getcwd()))
         self.elem = []
 
         if len(files) < self.max_length:
@@ -96,16 +128,31 @@ class CustomLabel(QLabel):
         for i in range(len(files)):
             self.elem.append(ListElement(files[i], self, (30, 30+30*i)))
 
+        #add back button
+        self.back = QPushButton(" <--", self)
+        self.back.setToolTip('Go up a directory')
+        self.back.move(0,0)
+        self.back.clicked.connect(self.back_function)
+
+    @pyqtSlot()
+    def back_function(self):
+        os.chdir("..")
+        self.parent.cur_dir = os.getcwd()
+        self.update_dir()
+
     def dragEnterEvent(self, e):
         print("File has entered the drop area")
         e.accept()
 
     def dropEvent(self, e):
         #e.mimeData().text()
-        location = e.mimeData().text()
+        fileName = e.mimeData().text()
+        self.parent.cur_file = fileName
+        self.parent.selected_file.setText("The selected file is: " + self.parent.cur_file)
+        self.parent.selected_file.adjustSize()
 
     def update_dir(self):
-        files = os.listdir(os.getcwd())
+        files = parse_for_hidden(os.listdir(os.getcwd()))
 
         if len(files) < self.max_length:
             files += [""]*(self.max_length-len(files))
@@ -125,25 +172,86 @@ class ListElement(QPushButton):
 
     def enterEvent(self, QEvent):
         if self.text() != "":
-            self.setStyleSheet("color: rgb(255, 255, 255); background-color: rgb(0,0,0); border:0px solid rgb(0, 0, 0);")
+            self.setStyleSheet("color: rgb(230, 230, 230); background-color: rgb(0,0,0); border:0px solid rgb(0, 0, 0);")
 
     def leaveEvent(self, QEvent):
         if self.text() != "":
-            self.setStyleSheet("color: rgb(0,0,0); background-color: rgb(255,255,255); border:0px solid rgb(0, 0, 0);")
+            self.setStyleSheet("color: rgb(0,0,0); background-color: rgb(230,230,230); border:0px solid rgb(0, 0, 0);")
 
     @pyqtSlot()
     def click(self):
         print(self.parent.parent.cur_file)
 
-        if not os.path.isdir(self.parent.parent.base_dir + "/" + self.text()):
-            self.parent.parent.cur_file = self.parent.parent.base_dir + "/" + self.text()
+        if not os.path.isdir(self.parent.parent.cur_dir + "/" + self.text()):
+            self.parent.parent.cur_file = self.parent.parent.cur_dir + "/" + self.text()
             self.parent.parent.selected_file.setText("The selected file is: " + self.parent.parent.cur_file)
             print("The selected file is: " + self.parent.parent.cur_file)
             self.parent.parent.selected_file.adjustSize()
         else:
-            os.chdir(self.parent.parent.base_dir + "/" + self.text())
-            self.parent.parent.base_dir = self.parent.parent.base_dir + "/" + self.text()
+            os.chdir(self.parent.parent.cur_dir + "/" + self.text())
+            self.parent.parent.cur_dir = self.parent.parent.cur_dir + "/" + self.text()
             self.parent.update_dir()
+
+
+class DirectoryPopup(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.title = 'Select Root Directory'
+        self.left = 100
+        self.top = 100
+        self.width = 600
+        self.height = 400
+        self.parent = parent
+        self.directory = os.getcwd()
+
+        self.initUI()
+
+        self.show()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        #set into label
+        self.intro = QLabel(self)
+        self.intro.setText("This directory was not found to be a root directory\n\n please select a directory to use as the root")
+        self.intro.move(self.width//4,10)
+
+        self.confirm = QPushButton("Confirm", self)
+        self.confirm.setToolTip("Click to confirm your root directory. By default it will be your current directory")
+        self.confirm.move(self.width//(3.5), self.height//(4/3))
+        self.confirm.clicked.connect(self.confirm_func)
+
+        self.find = QPushButton("Find", self)
+        self.find.setToolTip("Click to find a new directory to select as your root")
+        self.find.move(self.width//(1.8), self.height//(4/3))
+        self.find.clicked.connect(self.choose_dir)
+
+        self.chosen_dir = QLabel(self)
+        self.chosen_dir.setText("Selected Directory: " + self.directory)
+        self.chosen_dir.move(self.width // 2 - 6*len("Selected Directory: " + self.directory)//2, self.height//2)
+
+    def confirm_func(self, event):
+        self.parent.base_dir = self.directory
+        self.parent.cur_dir = self.directory
+        os.chdir(self.directory)
+        self.parent.drag_object.update_dir()
+        self.parent.directory_label.setText("Root Directory: " + self.parent.base_dir)
+        self.parent.directory_label.adjustSize()
+        if not os.path.isdir(self.directory + "/.hidden"):
+            os.system("mkdir " + self.directory + "/.hidden")
+        self.close()
+
+    def choose_dir(self, event):
+        fileName = str(QFileDialog.getExistingDirectory(self,"QFileDialog.getOpenFileName()"))
+
+        if fileName:
+            print(fileName)
+
+            if os.path.isdir(fileName):
+                self.directory = fileName
+                self.chosen_dir.setText("Selected Directory: " + self.directory)
+                self.chosen_dir.adjustSize()
 
 def main():
     app = QApplication(sys.argv)
